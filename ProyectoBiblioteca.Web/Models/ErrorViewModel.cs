729ecce646124a6becf6b9ffb2bd33397dac9ed7
dannyKentala <handres41@outlook.com>
2024-03-22 18:41:07 -0500

    Inicializar proyecto MVC

---
+++ ProyectoBiblioteca.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoBiblioteca.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
